8ac74b879e96bbf835767474a5d9a9bd1aa52591
RyanHWillis <ryan_h_willis@hotmail.com>
2016-09-24 12:21:25 +0100

    Initial Commit

---
+++ DemoInSwiftThree/HotelDetailContainerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HotelDetailContainerViewController.swift
+//  DemoInSwiftThree
+//
+//  Created by Ryan Willis on 24/09/2016.
+//  Copyright © 2016 RyanWillis. All rights reserved.
+//
+
+import UIKit
+
+class HotelDetailContainerViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: "HotelDetailContainerViewController", bundle: Bundle.main)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ DemoInSwiftThreeTests/DemoInSwiftThreeTests.swift
@@ -0,0 +1,36 @@
+//
+//  DemoInSwiftThreeTests.swift
+//  DemoInSwiftThreeTests
+//
+//  Created by Ryan Willis on 24/09/2016.
+//  Copyright © 2016 RyanWillis. All rights reserved.
+//
+
+import XCTest
+@testable import DemoInSwiftThree
+
+class DemoInSwiftThreeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
